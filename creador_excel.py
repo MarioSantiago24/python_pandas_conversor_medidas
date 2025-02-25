8564635ab2fb7f11ed0c8904752b1a45b76d530f
MarioSantiago24 <mariosantiagotrabajo2022@gmail.com>
2025-02-25 17:13:01 -0300

    Realizamos un programa en Python para convertir medidas

---
+++ creador_excel.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Dataframe es la información básica con el nombre de la piezas y centímetros para poder armar el Excel
+
+data = {
+    "Piezas:" : ["Pata" , "Tablero", " Puerta", "Estante", "Panel Lateral"],
+    "Centímetros:" : [40,120,60,30,180]
+}
+
+df = pd.DataFrame(data)
+
+# Guardar el DataFrame en un archivo Excel
+
+df.to_excel("muebles_medidas.xlsx", index=False)
+
